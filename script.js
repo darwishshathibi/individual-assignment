459cee0e9f99a1413733ff45acf6969c3fa15349
darwishathibi <danish.darwis@live.iium.edu.my>
2023-12-04 14:33:23 +0800

    add submit disabled features

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get the checkbox and select element
   var kulliyyahCheckbox = document.getElementById("kulliyyah");
   var kulliyyahSelect = document.querySelector(".form-select[aria-placeholder='Kulliyyah']");
+  var submitButton = document.getElementById("submit_button");
+  var permissionCheckbox = document.getElementById("permission");
 
   kulliyyahSelect.disabled = true;
+  submitButton.disabled = true;
+
+  permissionCheckbox.addEventListener("change", function () {
+    submitButton.disabled = !permissionCheckbox.checked;
+  });
+
   // Add an event listener to the checkbox
   kulliyyahCheckbox.addEventListener("change", function () {
       // Enable or disable the select based on the checkbox state
